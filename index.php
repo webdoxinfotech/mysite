36f4a02cd927819d77d1ec3e474d7f29484020da
webdox <test.webdox@gmail.com>
2019-06-17 16:54:13 +0530

    This is my first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<style type="text/css">
+		
+		h1{
+			background: red;
+			font-size: 2em;
+			display: inline;
+		}
+	</style>
+	<title></title>
+</head>
+<body>
+	<?php
+		$a = 10;
+		$b = 10;
+		$c = $a + $b;
+
+		$msg= "<h1> sum is $c </h1>";
+		echo $msg;
+
+
+	?>
+
+</body>
+</html>
